d10ef0dcf6099815067794925c5065d347fd03ab
Garrison Shepard <shepard.garrisont@gmail.com>
2019-06-11 00:35:47 -0400

    link data models

--- data_model/seq.js
+++ data_model/seq.js
@@ -1,8 +1,24 @@
+require('./.env');
 const Sequelize = require('sequelize');
-const Database = new Sequelize('postgres://garrison:GT$092894@localhost/campus_management');
+const database = new Sequelize('postgres://garrison:Gt$092894@localhost/campus_management');
 const CampusModel = require('./campus_model');
 const StudentModel = require('./student_model');
 
+//init data models
+const Campus = CampusModel(Sequelize, database);
+const Student = StudentModel(Sequelize, database);
 
-const Campus = CampusModel(Sequelize, Database);
-const Student = StudentModel(Sequelize, Database);
+//establish entity realtionships
+Student.belongsTo(Campus);
+Campus.hasMany(Student);
+
+//sync  db
+database.sync({force: true})
+        .then(() => {
+           console.log("db and tables created")
+});
+
+module.exports = {
+  Campus,
+  Student
+}
