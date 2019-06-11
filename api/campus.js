54830afb35af4d2e05abbf2df862c062ed0a0f63
Garrison Shepard <shepard.garrisont@gmail.com>
2019-06-11 16:06:16 -0400

    remove old comments

--- api/campus.js
+++ api/campus.js
@@ -9,24 +9,18 @@ const {Campus, Student} = require('../data_model/seq');
      Campus.findByPk(req.params.id).then((result) => res.status(201).json(result));
   });
 
-  //TODO: POST (add campus)
   router.post('/', (req, res) =>{
     Campus.create(req.body).then((result) => res.status(201).json(result));
   });
 
-  //TODO: PUT (update campus)
   router.put('/:id', (req, res) => {
       Campus.findByPk(req.params.id).then((campus) => campus.update(req.body)).then((updatedCampus) => res.status(201).json(updatedCampus))
   });
 
-  //TODO: DELETE (remove campus)
   router.delete('/:id', (req, res) => {
     Campus.destroy({
         where: { id: req.params.id }
     }).then(campus => res.status(201).json(campus));
   });
 
-
 module.exports = router
-//localhost:3000/api/v1/campuses/campus?id=0&name=nyu&address=23 wallabee way&imageUrl=http&description=GOOD
-//TODO:
