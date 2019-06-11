beef25cd25a8c64ea84ca628b12b5362ae90da26
Garrison Shepard <shepard.garrisont@gmail.com>
2019-06-11 14:37:28 -0400

    add campus REST interface

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./api');
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json());
 app.use("/api/v1", apiRouter);
 
 app.listen(3000, () => {
--- data_model/campus_model.js
+++ data_model/campus_model.js
@@ -1,11 +1,6 @@
 module.exports = (dataType, db) => {
 
   return  db.define('campus', {
-      id: {
-        type: dataType.INTEGER,
-        primaryKey: true,
-        autoincrement: true
-      },
       name: dataType.STRING,
       address: dataType.STRING,
       imageUrl: dataType.STRING,
